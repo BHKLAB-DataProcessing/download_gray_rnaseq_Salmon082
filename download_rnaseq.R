ecd23ff079e9f8b61624234c4f3e0613aa648e27
imanthonny <34114220+imanthonny@users.noreply.github.com>
2020-02-26 12:09:58 -0500

    Create download_rnaseq.R

---
+++ download_rnaseq.R
@@ -0,0 +1,5 @@
+library(downloader)
+basePath <- "https://orcestradata.blob.core.windows.net/gray/GRAY/2017/RNA-seq"
+download(file.path(basePath, "Salmon_0.8.2.tar.gz"), destfile="/pfs/out/Salmon_0.8.2.tar.gz")
+untar("/pfs/out/Salmon_0.8.2.tar.gz", exdir="/pfs/out/Salmon_0.8.2")
+file.remove("/pfs/out/Salmon_0.8.2.tar.gz")
